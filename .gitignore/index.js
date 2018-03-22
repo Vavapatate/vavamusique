b2cf5eab984adf0dcba33205a090b8108aba58e3
Vavapatate <37655679+Vavapatate@users.noreply.github.com>
2018-03-22 18:21:28 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,6 +5,11 @@ var bot = new Discord.Client();
 bot.on ("ready", function() {
     bot.user.setGames("vavaMusique, !help");
     console.log("Le Bot est ON")
-});
+})
 
-bot.login("NDI2MDkwMDk4OTkyMzQ5MTg0.DZT97Q.vzOT8QszQIHxCw1HPNaIHNd_hL0");
+bot.on("message", message => {
+    if(message.content === "!ping"){
+ message.reply("Pong! Lel")
+})
+}
+bot.login("NDI2MDkwMDk4OTkyMzQ5MTg0.DZVxnw.ZcrMNkdeqyCKpNMG4N8ZSlpSf14")
